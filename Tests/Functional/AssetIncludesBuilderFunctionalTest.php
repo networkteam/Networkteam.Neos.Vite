1cf70503e13f6f266d6e66b031c2406a0762f273
Lukas Neugebauer <25110308+lneugebauer@users.noreply.github.com>
2024-09-18 19:43:39 +0200

    Update tests

--- Tests/Functional/AssetIncludesBuilderFunctionalTest.php
+++ Tests/Functional/AssetIncludesBuilderFunctionalTest.php
@@ -49,7 +49,7 @@ class AssetIncludesBuilderFunctionalTest extends FunctionalTestCase
             '<link rel="stylesheet" href="http://localhost/_Resources/Testing/Static/Packages/Networkteam.Neos.Vite/Dist/assets/main.b82dbe22.css">' . PHP_EOL .
             '<link rel="stylesheet" href="http://localhost/_Resources/Testing/Static/Packages/Networkteam.Neos.Vite/Dist/assets/shared.a834bfc3.css">' . PHP_EOL .
             '<script type="module" src="http://localhost/_Resources/Testing/Static/Packages/Networkteam.Neos.Vite/Dist/assets/main.4889e940.js"></script>' . PHP_EOL .
-            '<script type="modulepreload" src="http://localhost/_Resources/Testing/Static/Packages/Networkteam.Neos.Vite/Dist/assets/shared.83069a53.js"></script>'
+            '<link rel="modulepreload" href="http://localhost/_Resources/Testing/Static/Packages/Networkteam.Neos.Vite/Dist/assets/shared.83069a53.js">'
             , $html, 'should create correct includes for main entry');
 
         $file = $builder->productionUrl('main.js');
@@ -59,7 +59,7 @@ class AssetIncludesBuilderFunctionalTest extends FunctionalTestCase
         $this->assertEquals(
             '<link rel="stylesheet" href="http://localhost/_Resources/Testing/Static/Packages/Networkteam.Neos.Vite/Dist/assets/shared.a834bfc3.css">' . PHP_EOL .
             '<script type="module" src="http://localhost/_Resources/Testing/Static/Packages/Networkteam.Neos.Vite/Dist/assets/foo.869aea0d.js"></script>' . PHP_EOL .
-            '<script type="modulepreload" src="http://localhost/_Resources/Testing/Static/Packages/Networkteam.Neos.Vite/Dist/assets/shared.83069a53.js"></script>'
+            '<link rel="modulepreload" href="http://localhost/_Resources/Testing/Static/Packages/Networkteam.Neos.Vite/Dist/assets/shared.83069a53.js">'
             , $html, 'should create correct includes for other entries');
 
         $file = $builder->productionUrl('views/foo.js');
